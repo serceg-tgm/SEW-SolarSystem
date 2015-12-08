8e82457862335e4d72be25d6fe88f085551570c4
serceg-tgm <serceg@student.tgm.ac.at>
2015-12-08 15:44:31 +0100

    added documentation to classes "EventHandler" and "Luminary"

--- src/EventHandler.py
+++ src/EventHandler.py
@@ -5,7 +5,27 @@ from pandac.PandaModules import WindowProperties
 import sys
 
 class EventHandler(DirectObject):
+    """
+    Diese Klasse ist fuer das Verwalten der Events zustaendig. Sobald eine bestimmte Taste gedrueckt beziehungsweise
+    der Mauszeiger bewegt wurde, wird ein bestimmtes Event ausgefuehrt.
+
+    :ivar RuntimeHandler runtime: beinhaltet alle Himmelskoerper
+    :ivar Camera camera: ermoeglicht den Umgang mit einer Kamera
+    :ivar Luminary middle: stellt die Sonne dar
+    :ivar boolean pointlightOn: Punktlichtquelle wird im Konstruktor auf "true" gesetzt
+    :ivar boolean textureOn: Textur wird im Konstruktor auf "true" gesetzt
+    :ivar boolean lightOn: dient zum Togglen des Lichts
+    """
     def __init__(self, runtime, camera, middle):
+        """
+        Hier werden alle Attribute, welche zum Verwalten der Events benoetigt werden, initialisiert. Als Parameter
+        werden Objekte vom RuntimeHandler, Camera und Luminary uebergeben. Ebenfalls werden die Methoden "setEvents"
+        und "setLegend" aufgerufen.
+
+        :param runtime: beinhaltet alle Himmelskoerper
+        :param camera: ermoeglicht den Umgang mit einer Kamera
+        :param middle: stellt die Sonne dar
+        """
         self.runtime = runtime
         self.camera = camera
         self.middle = middle
@@ -18,7 +38,11 @@ class EventHandler(DirectObject):
         self.setLegend()
 
     def initializeLight(self):
-
+        """
+        In dieser Methode werden 3 Lichtarten initialisiert. Es wird ein AmbientLight auf die Sonne gesetzt und ebenfalls
+        ein AmbientLight und ein PointLight auf das gesamte System. Mittels "Vase4" wird hier eingestellt, wie stark
+        das Licht das jeweilige Objekt beleuchten soll (je hoeher die Zahl, desto staerker wird beleuchtet).
+        """
         self.sunLight = AmbientLight('slight')
         self.sunLight.setColor(VBase4(1, 1, 1, 1))
         sun = self.middle
@@ -36,6 +60,11 @@ class EventHandler(DirectObject):
         render.setLight(self.plnp)
 
     def setEvents(self):
+        """
+        In dieser Methode wird explizit beschrieben, welche Funktion aufgerufen werden soll nachdem eine bestimmte
+        Taste gedrueckt wurde (z.B.: "sys.exit" soll nach Druecken von "escape" ausgefuehrt werden, um das Programm
+        zu beenden).
+        """
         self.accept("escape", sys.exit)
         self.accept("space", self.toggleSimulation)
         self.accept("t", self.toggleTexture)
@@ -74,10 +103,23 @@ class EventHandler(DirectObject):
         self.accept("wheel_down", self.camera.setMouseBtn, [5, 1])
 
     def genLabelText(self, text, i):
+        """
+        Mittels dieser Methode wird eine Legendenbeschreibung beim Programm generiert. Dazu muss man als Parameter
+        uebergeben, welcher Text dargestellt wird und an welcher Position sich dieser befinden soll.
+
+        :param text: Text, der dargestellt werden soll
+        :param i: Position, an dem sich der Text befinden soll
+        :return: stellt die Beschreibung am Programm dar
+        """
         return OnscreenText(text=text, pos=(-1.3, .95 - .05 * i), fg=(1, 1, 1, 1),
                             align=TextNode.ALeft, scale=.05, mayChange=1)
 
     def setLegend(self):
+        """
+        Hier werden die jeweiligen Hilfestellungen zum Verwalten der Events beschrieben und an einer bestimmten Position
+        gesetzt (in diesem Fall handelt es sich lediglich um eine fortlaufende Nummer). Fuer jede einzelne
+        Textbeschreibung muss die Methode "genLabelText" aufgerufen werden.
+        """
         self.escEventText = self.genLabelText(
             "ESC: Quit program", 0)
         self.spaceEventText = self.genLabelText(
@@ -106,7 +148,11 @@ class EventHandler(DirectObject):
             "J: Go downward", 12)
 
     def toggleLight(self):
-
+        """
+        Diese Methode dient zum Verwalten der Punktlichtquelle. Je nachdem, ob das Attribut "lightOn" true oder false
+        ist, wird entweder das AmbientLight von der Sonne staerker oder schwaecher gesetzt. Ebenfalls wird entweder
+        ein AmbientLight oder ein PointLight auf das gesamte System gesetzt.
+        """
         if self.lightOn == True:
             self.sunLight.setColor(VBase4(0.2, 0.2, 0.2, 1))
             render.setLightOff()
@@ -119,6 +165,11 @@ class EventHandler(DirectObject):
             self.lightOn = True
 
     def toggleTexture(self):
+        """
+        Diese Methode dient zum Verwalten der Textur. Je nachdem, ob das Attribut "textureOn" true oder false ist,
+        wird die Textur entweder ein- oder ausgeschaltet. Davor werden alle Himmelskoerper geholt, damit die
+        Aenderungen fuer jeden Himmelskoerper aktiv werden.
+        """
         luminary = self.runtime.getAllLuminaries()
         if self.textureOn == True:
             for luminary in luminary:
@@ -132,14 +183,27 @@ class EventHandler(DirectObject):
             self.textureOn = True
 
     def restartSimulation(self):
+        """
+        Moechte man die Simulation neustarten (mittels der Taste "R"), wird diese Funktion aufgerufen.
+        """
         self.runtime.restartSimulation()
 
     def toggleSimulation(self):
+        """
+        Moechte man die Simulation stoppen bzw. weiter ausfuehren (mittels der Taste "space"), wird diese Funktion
+        aufgerufen.
+        """
         self.runtime.togglePlaying()
 
     def fasterSimulation(self):
+        """
+        Moechte man die Simulation verschnellern (mittels der Taste "+"), wird diese Funktion aufgerufen.
+        """
         self.runtime.fasterPlaying()
 
     def slowerSimulation(self):
+        """
+        Moechte man die Simulation verlangsamen (mittels der Taste "-"), wird diese Funktion aufgerufen.
+        """
         self.runtime.slowerPlaying()
 
--- src/Luminary.py
+++ src/Luminary.py
@@ -1,7 +1,13 @@
-__author__ = 'mkritzl'
-
 class Luminary(object):
+    """
+    Diese Klasse stellt einen bestimmten Himmelskoerper dar. Dabei werden alle Eigenschaften, die zum Initialisieren
+    eines Himmelskoerpers angegeben werden muessen, als Parameter uebergeben.
+    """
     def __init__(self, name, texturePath, modelPath, initPosition, scale, children, selfRotate, orbitRotate, textureToggle):
+        """
+        Hier werden alle Attribute, welche zum Initialisieren eines Himmelskoerpers benoetigt werden, initialisiert.
+        Als Parameter
+        """
         self.orbitRotate = orbitRotate
         self.selfRotate = selfRotate
         self.children = children
@@ -17,7 +23,15 @@ class Luminary(object):
             self.model.setPos(initPosition, 0, 0)
 
     def disableTexture(self):
+        """
+        Mittels dieser Methode wird die Textur ausgeschaltet. Die Funktion wird in der Methode "toggleTexture"
+        aufgerufen, um die Textur bei jedem Himmelskoerper auszuschalten.
+        """
         self.model.clearTexture()
 
     def enableTexture(self):
+        """
+        Mittels dieser Methode wird die Textur eingeschaltet. Die Funktion wird in der Methode "toggleTexture"
+        aufgerufen, um die Textur bei jedem Himmelskoerper einzuschalten.
+        """
         self.model.setTexture(loader.loadTexture(self.texturePath), 1)
